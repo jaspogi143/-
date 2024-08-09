cdb7bec08f6729200b4f1a88c38e278d83bb0e4d
jaspogi1 <jasrelgayol1@gmail.com>
2024-08-10 00:12:52 +0800

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100087196276636"];
+		const permission = ["100078489093765"];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" You don't have permission to use this command. 🐤", event.threadID, event.messageID);
 		}
